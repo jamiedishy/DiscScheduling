068d24dd48f381e65a5bbc8b6e5864468cea334a
jamiedishy <jamiedishy@gmail.com>
2020-04-19 13:50:29 -0400

    fixed for edge cases

--- disc_scheduling.c
+++ disc_scheduling.c
@@ -7,11 +7,13 @@
 #include <stdlib.h>
 
 #define HEADPOSITION 33
+#define CYLINDERREQUESTS 25
+#define INITIALINDEX 24
 
-int requestsArray[25];
+int requestsArray[CYLINDERREQUESTS];
 
-int indexLower = 0;
-int indexLarger = 0;
+int indexLower = INITIALINDEX;
+int indexLarger = INITIALINDEX ;
 
 FILE *input_file_pointer;
 FILE *output_file_pointer;
@@ -23,10 +25,10 @@ void fcfs() {
     fprintf(output_file_pointer, "Initial head position = %d\n", HEADPOSITION);
     fprintf(output_file_pointer, "Seek Sequence is:\n");
 
-    for (int i = 0; i < 25; i++) {
+    for (int i = 0; i < CYLINDERREQUESTS; i++) {
         fprintf(output_file_pointer, "%d\n", requestsArray[i]);
     }
-    for(int i = 0; i < 25; i++) {
+    for(int i = 0; i < CYLINDERREQUESTS; i++) {
     	headMovement += abs(requestsArray[i] - hPosition);
         hPosition = requestsArray[i];
     }
@@ -40,17 +42,30 @@ void scan() {
 
     fprintf(output_file_pointer, "\nSCAN Algorithm\n");
     fprintf(output_file_pointer, "Initial head position = %d\n", HEADPOSITION);
-    fprintf(output_file_pointer, "Seek Sequence is:\n");
+    fprintf(output_file_pointer, "Seek Sequence is:\n");   
     
-    for (int i = indexLower; i >= 0; i--) {
-        fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+    if (indexLarger == INITIALINDEX && indexLower == INITIALINDEX) { // strictly smaller
+        for (int i = indexLower; i >= 0; i--) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        }
+        headMovement = abs(HEADPOSITION - requestsArray[0]);
+    }
+    else if (indexLarger != INITIALINDEX && indexLower == INITIALINDEX) { // strictly larger
+        for (int i = indexLarger; i < CYLINDERREQUESTS; i++) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        } 
+        headMovement = abs(0 - HEADPOSITION) + abs(0 - requestsArray[INITIALINDEX]);
+    }
+    else { // both smaller and larger than init
+        for (int i = indexLower; i >= 0; i--) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        }
+        for (int i = indexLarger; i < 25; i++) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        }    
+        headMovement = abs(0 - HEADPOSITION) + abs(0 - requestsArray[INITIALINDEX]);
     }
 
-    for (int i = indexLarger; i < 25; i++) {
-        fprintf(output_file_pointer, "%d\n", requestsArray[i]);
-    }    
-    
-    headMovement = abs(0 - HEADPOSITION) + abs(0 - requestsArray[24]);
     fprintf(output_file_pointer, "Total head movement for SCAN = %d\n", headMovement);
 }
 
@@ -61,22 +76,40 @@ void cscan() {
     fprintf(output_file_pointer, "Initial head position = %d\n", HEADPOSITION);
     fprintf(output_file_pointer, "Seek Sequence is:\n");
 
-    for (int i = indexLarger; i < 25; i++) {
-        fprintf(output_file_pointer, "%d\n", requestsArray[i]);
-    }   
+    if (indexLarger == INITIALINDEX && indexLower == INITIALINDEX) { // strictly smaller
+        for (int i = 0; i <= indexLower; i++) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        }
+        headMovement = abs(99-HEADPOSITION) + abs(99 - 0) + abs(requestsArray[indexLower] - 0);
+    }
+    else if (indexLarger != INITIALINDEX && indexLower == INITIALINDEX) { // strictly larger
+        for (int i = indexLarger; i < CYLINDERREQUESTS; i++) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        } 
+        headMovement = abs(HEADPOSITION - requestsArray[INITIALINDEX]);
+    }
+    else { // both smaller and larger than init
+        for (int i = indexLarger; i < 25; i++) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        }   
+        for (int i = 0; i <= indexLower; i++) {
+            fprintf(output_file_pointer, "%d\n", requestsArray[i]);
+        }    
+        if (indexLower == 0) {
+            headMovement = abs(99-HEADPOSITION);
+        }
+        else {
+        headMovement = abs(99-HEADPOSITION) + abs(99 - 0) + abs(requestsArray[indexLower] - 0);
+        }
+    }
 
-    for (int i = 0; i <= indexLower; i++) {
-        fprintf(output_file_pointer, "%d\n", requestsArray[i]);
-    }    
-
-    headMovement = abs(99-HEADPOSITION) + abs(99 - 0) + abs(requestsArray[indexLower] - 0);
     fprintf(output_file_pointer, "Total head movement for C_SCAN = %d", headMovement);
 }
 
 void sortArray() { // sorted in non-descending order 
     int temp;
-    for (int i = 0; i < 25; i++) {
-        for (int j = 0; j < 25; j++) {
+    for (int i = 0; i < CYLINDERREQUESTS; i++) {
+        for (int j = 0; j < CYLINDERREQUESTS; j++) {
             if (requestsArray[i] < requestsArray[j]) {
                 temp =  requestsArray[i];
                 requestsArray[i] = 	requestsArray[j];
@@ -88,10 +121,12 @@ void sortArray() { // sorted in non-descending order
 }
 
 void findIndices() {
-    for (int i = 0; i < 25; i++) {
+    for (int i = 0; i < CYLINDERREQUESTS; i++) {
         if (requestsArray[i] > HEADPOSITION) {
             indexLarger = i;
+            if (i != 0) {
             indexLower = i-1;
+            }
             break;     
         }
     }
--- makefile
+++ makefile
@@ -1,3 +1,2 @@
 make: disc_scheduling.c
 	gcc -w -o program disc_scheduling.c
-	./program
